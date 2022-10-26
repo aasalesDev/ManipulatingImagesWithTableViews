3f3add93c073596505b9838591eb85ae09970485
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-26 10:31:01 -0300

    Added navigation item

--- ManipulatingImagesWithViewTables/ManipulatingImagesWithViewTables/Detail View Controller/DetailViewController.swift
+++ ManipulatingImagesWithViewTables/ManipulatingImagesWithViewTables/Detail View Controller/DetailViewController.swift
@@ -16,7 +16,31 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+    }
+    
+    func configureView(){
         flagImageView.image = image
         title = myTitle?.uppercased()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        //It is needed to go to the file info.plist (right click -> add row) -> privacy - Photo Library Addition....
+        guard let image = flagImageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }
